7276a0784084603f2972ec17eb7fb1638e958d75
mdatsev <martin.datsev.p@gmail.com>
2018-11-17 21:42:02 +0200

    spawning and destroying agents and predator and prey simulation

--- AgentBased.js
+++ AgentBased.js
@@ -22,11 +22,14 @@ class AgentBased {
         this.frameCounter = new FrameCounter(100)
         this.renderer = new ABRenderer(this, canvas)
         this.agents = []
+        this.toSpawn = []
+        this.toDestroy = []
     }
 
     spreadRandom(agent, n, startx = this.origin.x, starty = this.origin.y, endx = this.width * this.scale, endy = this.height * this.scale) {
         for(let i = 0; i < n; i++) {
-            this.agents.push(new (agent)(random(startx, endx), random(starty, endy)))
+            const len = this.agents.push(new (agent)(random(startx, endx), random(starty, endy), this))
+            this.agents[len - 1].init()
         }
     }
 
@@ -39,6 +42,18 @@ class AgentBased {
         requestAnimationFrame(this.startLoop.bind(this))
     }
 
+    spawn(agent, x, y) {
+        this.toSpawn.push({agent, x, y})
+    }
+
+    destroy(agent) {
+        for(let i = 0; i < this.agents.length; i++) {
+            if(this.agents[i]._ssinternal.original == agent) {
+                this.toDestroy.push(i)
+            }
+        }
+    }
+
     tick() {
         this.renderer.draw()
 
@@ -46,27 +61,67 @@ class AgentBased {
     
         for(let i = 0; i < this.agents.length; i++) {
             this.old_agents.push(Object.assign(new (this.agents[i].constructor)(), this.agents[i]))
+            this.old_agents[i]._ssinternal.original = this.agents[i]
         }
 
         for(let i = 0; i < this.agents.length; i++) {
             this.agents[i].update()
         }
+        
+        for(const a of this.toSpawn) {
+            this.agents.push(new (a.agent)(a.x, a.y, this))
+        }
 
+        for(const a of this.toSpawn) {
+            this.agents.push(new (a.agent)(a.x, a.y, this))
+        }
+        this.toSpawn = []
+
+        this.toDestroy = [...new Set(this.toDestroy)]
+        this.toDestroy.sort((a, b) => b - a)
+        for(const i of this.toDestroy)
+            this.agents.splice(i, 1)
+        this.toDestroy = []
         this.frameCounter.update()
-        console.log(this.frameCounter.getFramerate())
+        //console.log(this.frameCounter.getFramerate())
+    }
+
+    neighsWithin(agent, dist, type) {
+        const neighs = []
+        for(const a of this.agents) {
+            const x = agent.x - a.x
+            const y = agent.y - a.y
+            const distSq = x * x + y * y
+            if(distSq <= dist * dist && a instanceof type) {
+                neighs.push(a)
+            }
+        }
+        return neighs
     }
 }
 
 class Agent {
-    constructor(x, y) {
+    constructor(x = 0, y = 0, ab = null) {
         this.x = x
         this.y = y
+        this.ab = ab
+        this._ssinternal = {}
     }
     move(x, y) {
         this.x += x
         this.y += y
     }
+    spawn(agent, x = this.x, y = this.y) {
+        this.ab.spawn(agent, x, y)
+    }
+    destroy(agent) {
+        this.ab.destroy(agent)
+    }
+    neighsWithin(dist, type) {
+        return this.ab.neighsWithin(this, dist, type)
+    }
     update() { }
+    init() { }
     getColor() { return '#ff00ff' }
 }
 
--- examples/FoxesAndRabbits.js
+++ examples/FoxesAndRabbits.js
@@ -1,9 +1,7 @@
 import {AgentBased, Agent} from '/AgentBased.js'
 
 window.ab = new AgentBased()
-
-const srandom = () => (.5 - Math.random()) * 10
-
+const srandom = () => (.5 - Math.random()) * 20
 class Rabbit extends Agent {
 
     getColor() {
@@ -11,28 +9,40 @@ class Rabbit extends Agent {
     }
 
     update() {
-        // if(Math.random() > .7)
-        //     this.spawn(Rabbit)
+        const r = Math.random()
+        if(r > .994)
+        {
+            this.spawn(Rabbit)
+        }
         this.move(srandom(), srandom())
     }
 }
 
 class Fox extends Agent {
 
+    init() {
+        this.hunger = 0
+    }
+
     getColor() {
         return '#ff0000'
     }
 
     update() {
-        // this.neighsWithin(5, Rabbit).forEach(a => {
-        //     this.destroy(a)
-        //     if(Math.random() > .7)
-        //         this.spawn(Fox)
-        // })
+        this.hunger++
+        if(this.hunger > 4) {
+            this.destroy(this)
+        }
+        this.neighsWithin(10, Rabbit).forEach(a => {
+            this.destroy(a)
+            if(Math.random() > .9)
+                this.spawn(Fox)
+            this.hunger = 0
+        })
         this.move(srandom(), srandom())
     }
 }
 
 ab.spreadRandom(Fox, 10)
-ab.spreadRandom(Rabbit, 10)
+ab.spreadRandom(Rabbit, 2000)
 ab.run()
