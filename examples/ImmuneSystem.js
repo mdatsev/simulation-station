d8760c70e82202f7289cdc10b76285cdc9528526
mdatsev <martin.datsev.p@gmail.com>
2019-02-24 01:20:23 +0200

    refactored immune system

--- examples/ImmuneSystem.js
+++ examples/ImmuneSystem.js
@@ -1,9 +1,8 @@
-import {CALayer, ABLayer, Cell, Agent, Simulation, simpleValueCell} from '../Simulation.js'
+import {CALayer, ABLayer, Cell, Agent, Simulation, makeCell} from '../Simulation.js'
 import {createLayerControls, createTimeControls} from '../UI.js'
-import {chance, random} from '../util.js'
+import {chance, random, weightedChooseDo} from '../util.js'
 
 const params = {
-
     pathoMax: 200,
     chemoMax: 1000,
     tissueMax: 1000,
@@ -20,180 +19,128 @@ const params = {
     followPathoProb: 0.5,
     followInfectionProb: 0.5,
     strayProb: 0.4,
-    mastStrayProb: 0.6,
+    mastStrayProb: 0.6
 }
 
-const random_dir = _ => [1-Math.floor(Math.random() * 3), 1-Math.floor(Math.random() * 3)]
+
+const random_dir = _ => [Math.floor(2-Math.random() * 3), Math.floor(2-Math.random() * 3)]
 
 class Dead extends Cell {
-    getColor() {
-        return [0,0,0]
-    }
+    add(){}
+    getColor() { return [0,0,0] }
     update() {
-        const regenerateProb = 1 * params.regenCoef; //todo use inert cell count
-        if(chance(regenerateProb))
+        //todo use inert cell count
+        if(chance(params.regenCoef))
             this.become(Alive);
     }
 }
 
 
-class Alive extends simpleValueCell([255, 230, 230], [255, 0, 0], params.tissueMax) {
-    update() { 
-        const infectProb = this.on(patho).value * params.infectionProbCoef;
-        if(this.value === 0) {
-            if(chance(infectProb))
-                this.add(1)
-        } else if(this.value > 0) {
+class Alive extends makeCell({
+        minColor: [255, 230, 230], 
+        maxColor:[255, 0, 0], 
+        maxValue: params.tissueMax}) {
+    update() {
+        if(this > 0) {
             this.add(1)
             if(chance(params.infectionSpreadProb))
-            {
-                const n = this.getRandomNeigh()
-                if(n instanceof Alive) {
-                    n.add(1)
-                }
-            }    
-
-            if(this.value > 500) {
-                const deathProb = this.value * params.deathProb;
-                if (chance(deathProb))
-                    this.become(Dead)
+                this.getRandomNeigh().add(1)
+            if(this > params.tissueMax / 2 && chance(this * params.deathProb)) {
+                this.become(Dead)
             }
-        } else {
-            throw Error(`value is ${this.value}`)
+        } else if(chance(this.on(patho) * params.infectionProbCoef)) {
+            this.add(1)
         }
     }
 }
 
 const tissue = new CALayer(Alive)
 
-const chemo = new CALayer(class extends simpleValueCell([153, 0, 51], [255, 255, 102], params.chemoMax) {
-    update() {
-        let basePortion = 0;
 
-        const neighs = this.getMooreNeighbours()
-        const dist = this.getMooreDistances()
-        for(let i = 0; i < neighs.length; i++) {
-            if(this.value > neighs[i].value)
-                basePortion += dist[i]
-        }
-        for(let i = 0; i < neighs.length; i++) {
-            if(this.value > neighs[i].value)
-            {
-                const portion = basePortion * dist[i];
-                
-                const amount = (this.value - neighs[i].value) / Math.floor(portion);
-                neighs[i].curr.add(amount);
-                this.add(-amount);
-            }
-        }
-            
-        // const neighs = this.getMooreNeighbours()
-        // this.add(neighs.map(n => n.value).reduce((a, b) => a + b) / 8 - this.value)
-        if(this.value > params.chemoDecayMin) {  
-            this.add(-(this.value * params.chemoDecayCoef + params.chemoDecayMin))
-        }
-    }
-})
 
-const patho = new CALayer(class extends simpleValueCell([255, 255, 255], [255, 0, 0], params.pathoMax) {
+const chemo = new CALayer(makeCell({
+    minColor: [153, 0, 51], 
+    maxColor:[255, 255, 102], 
+    maxValue: params.chemoMax,
+    diffuse: true,
+    decayBase:  params.chemoDecayMin,
+    decay: params.chemoDecayCoef})
+)
+
+const patho = new CALayer(class extends makeCell({
+    minColor: [255, 255, 255], 
+    maxColor: [255, 0, 0], 
+    maxValue: params.pathoMax}) {
     update() {
-        if(chance(params.pathoProb) &&
-                this.value < params.pathoMax)
+        if(chance(params.pathoProb))
             this.add(200);
 
-        //Math.round(Math.random()), Math.round(Math.random())
-        const low = params.pathoMoveMinRatio;
-        const high = params.pathoMoveMaxRatio;
-
-        const amount = random(low, high) * this.value;
-        
+        const amount = this * random(params.pathoMoveMinRatio, params.pathoMoveMaxRatio);
         this.add(-amount)
-
         this.getRandomNeigh().add(amount)
     }
 })
 
-class Mast extends Agent {
+class ImmuneCell extends Agent {
+    most(layer) {
+        const neighs = this.on(layer).getMooreNeighbours()
+        let most = 0;
+        for(const n of neighs) {
+            if(n.value > most) {
+                most = n;
+            }
+        }
+        return most == 0 ? random(neighs) : most
+    }
+    followPatho() { this.moveTo(this.most(patho)) }
+    followChemo() { this.moveTo(this.most(patho)) }
+    followInfect() { this.moveTo(this.most(tissue)) }
+    stray() { this.move(random_dir()) }
+}
+
+class Mast extends ImmuneCell {
     update() {
         const pathoCell = this.on(patho)
         const pathoNeighs = pathoCell.getVonNeumannNeighbours()
-        const sum = pathoNeighs.reduce((a, e) => a + e.value, pathoCell.value)
-        pathoCell.add(-Math.floor(pathoCell.value * 0.3))
-        if(sum > 0 && this.on(chemo).value < 1000)
+        const sum = pathoNeighs.reduce((a, e) => a + e)
+        pathoCell.mult(0.7)
+        if(sum > 0)
             this.on(chemo).add(sum * 500);
 
-        if(chance(params.mastStrayProb))
-            this.move(...random_dir());
-        else
-        {
-            const most = pathoNeighs.reduce((a, b) => a.value > b.value ? a : b)
-            this.x = most.x
-            this.y = most.y
-        }
+        weightedChooseDo(
+            [params.mastStrayProb, _=>this.stray()],
+            [1-params.mastStrayProb, _=>this.followPatho()])
     }
     
-    static getTexture() {
-        return './mast.png'
-    }
+    static getTexture() { return './mast.png' }
 }
 
-class Macrophage extends Agent {
+class Macrophage extends ImmuneCell {
     update() {
         const pathoCell = this.on(patho)
-        pathoCell.add(-Math.floor(pathoCell.value * 0.9))
-        const chemoCell = this.on(chemo)
-        const r = Math.random();
+        pathoCell.mult(0.1)
+        weightedChooseDo(
+            [params.strayProb, _=>this.stray()],
+            [params.followChemoProb, _=>this.followChemo()],
+            [params.followPathoProb, _=>this.followPatho()])
+    }
 
-        if(r < params.strayProb)
-            this.move(...random_dir());
-        else if(r < (params.strayProb+params.followChemoProb))
-        {
-            const most = chemoCell.getMooreNeighbours().reduce((a, b) => a.value > b.value ? a : b)
-            this.x = most.x
-            this.y = most.y
-        }
-        else if(r < (params.strayProb+params.followChemoProb+params.followPathoProb))
-        {
-            const most = pathoCell.getVonNeumannNeighbours().reduce((a, b) => a.value > b.value ? a : b)
-            this.x = most.x
-            this.y = most.y
-        }
-    }
-    
-    static getTexture() {
-        return './macrophage.png'
-    }
+    static getTexture() { return './macrophage.png' }
 }
 
-class NaturalKiller extends Agent {
+class NaturalKiller extends ImmuneCell {
     update() {
-
-        if(this.on(tissue).value > 0) {
+        if(this.on(tissue) > 0) {
             this.on(tissue).become(Dead)
         }
-        const pathoCell = this.on(patho)
-        const chemoCell = this.on(chemo)
-        const r = Math.random();
 
-        if(r < params.strayProb)
-            this.move(...random_dir());
-        else if(r < (params.strayProb+params.followChemoProb))
-        {
-            const most = chemoCell.getMooreNeighbours().reduce((a, b) => a.value > b.value ? a : b)
-            this.x = most.x
-            this.y = most.y
-        }
-        else if(r < (params.strayProb+params.followChemoProb+params.followInfectionProb))
-        {
-            const most = pathoCell.getVonNeumannNeighbours().reduce((a, b) => a.value > b.value ? a : b)
-            this.x = most.x
-            this.y = most.y
-        }
+        weightedChooseDo(
+            [params.strayProb, _=>this.stray()],
+            [params.followChemoProb, _=>this.followChemo()],
+            [params.followInfectionProb, _=>this.followInfect()])
     }
     
-    static getTexture() {
-        return './nk.png'
-    }
+    static getTexture() { return './nk.png' }
 }
 
 const mast = new ABLayer()
@@ -202,9 +149,9 @@ const killer = new ABLayer()
 
 new Simulation(100, 100, [patho, chemo, tissue, mast, macro, killer], {scale: 8,
 init: sim => {
-    mast.spreadRandom(Mast, 100)
-    macro.spreadRandom(Macrophage, 100)
-    killer.spreadRandom(NaturalKiller, 200)
+    mast.spreadRandom(Mast, 40)
+    macro.spreadRandom(Macrophage, 40)
+    killer.spreadRandom(NaturalKiller, 80)
     sim.resume()
     createLayerControls(sim)
     createTimeControls(sim)
