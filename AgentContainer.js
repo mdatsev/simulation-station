b45750f6971f9f259eed7056c227278e62b72c36
mdatsev <martin.datsev.p@gmail.com>
2019-03-07 19:54:29 +0200

    infinitely expandable quadtree

--- AgentContainer.js
+++ AgentContainer.js
@@ -51,39 +51,76 @@ class Circle {
   }
 
 class QuadTree {
-    constructor(boundary, capacity = 1) {
+    constructor(boundary, capacity = 1, isRoot = true) {
         this.boundary = boundary
         this.capacity = capacity
         this.agents = []
+        if(isRoot)
+            this.isRoot = true
     }
 
     makeChild(xoffset, yoffset) {
         return new QuadTree(new Rectangle(this.boundary.x + xoffset * this.boundary.w / 2, this.boundary.y + yoffset * this.boundary.h / 2, this.boundary.w / 2, this.boundary.h / 2), this.capacity)
     }
 
+    makeParent(xoffset, yoffset, prop) {
+        this.isRoot = false
+        const parent = new QuadTree(new Rectangle(this.boundary.x - xoffset * this.boundary.w, this.boundary.y - yoffset * this.boundary.h, this.boundary.w * 2, this.boundary.h * 2), this.capacity, true)
+        parent[prop] = this
+        return parent
+    }
+
     divide() {
-        this.ne = this.makeChild(1, 0)
-        this.nw = this.makeChild(0, 0)
-        this.sw = this.makeChild(0, 1)
-        this.se = this.makeChild(1, 1)
+        if(!this.ne) this.ne = this.makeChild(1, 0)
+        if(!this.nw) this.nw = this.makeChild(0, 0)
+        if(!this.sw) this.sw = this.makeChild(0, 1)
+        if(!this.se) this.se = this.makeChild(1, 1)
         this.divided = true
     }
 
     insert(p) {
         if(!this.boundary.contains(p))
-            return false
-        if(this.agents.length < this.capacity){
-            this.agents.push(p)
+        {
+            if(this.isRoot) {
+                let curr = this,
+                    parent
+                const expand = (function() {
+                    let {x, y} = curr.boundary
+                    if(p.x < x && p.y < y)
+                        return [1, 1, 'se']
+                    else if(p.x >= x && p.y < y)
+                        return [0, 1, 'sw']
+                    else if(p.x < x && p.y >= y)
+                        return [1, 0, 'ne']
+                    else if(p.x >= x && p.y >= y)
+                        return [0, 0, 'nw']
+                })()
+                do {
+                    parent = curr.makeParent(...expand)
+                    parent.divide()
+                    curr = parent
+                } while(!parent.boundary.contains(p))
+                return parent
+            } else {
+                return
+            }
         } else {
-            if(!this.divided)   
-                this.divide()
-            this.ne.insert(p)
-            this.nw.insert(p)
-            this.sw.insert(p)
-            this.se.insert(p)
+            if(this.agents.length < this.capacity){
+                this.agents.push(p)
+            } else {
+                this.overflow(p)
+            }
+            return this
         }
+    }
 
-        return true
+    overflow(p) {
+        if(!this.divided)   
+            this.divide()
+        this.ne.insert(p)
+        this.nw.insert(p)
+        this.sw.insert(p)
+        this.se.insert(p)
     }
 
     getInCircle(x, y, r) {
