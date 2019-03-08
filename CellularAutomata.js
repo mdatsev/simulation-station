60771ecd4974cc0574936acee40c31863915e5de
mdatsev <martin.datsev.p@gmail.com>
2019-03-08 20:56:14 +0200

    bugfix

--- CellularAutomata.js
+++ CellularAutomata.js
@@ -4,7 +4,7 @@ function CellularAutomata(nxCells, nyCells, cellTypes, simulation_options, layer
     const layer = new CALayer(cellTypes, layer_options)
     const sim = new Simulation(nxCells, nyCells, layer, simulation_options)
     return new Proxy({}, { get: (target, prop) => {
-        const get = o => o[prop].bind(o)
+        const get = o => (o[prop] instanceof Function) ? o[prop].bind(o) : o[prop]
         if(prop == 'layer') return layer
         if(prop == 'simulation') return sim
         if(prop in sim) return get(sim)
